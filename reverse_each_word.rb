c23c269187ba000505babd9fda50cd57c51bbca1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-28 04:56:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,8 +10,12 @@ def reverse_each_word_with_each(string)
 end
 
 def reverse_each_word(string)
+  array = string.split("")
+  test_array = []
+  array.collect do |string|
+    test_array << string.reverse
 end
-
+  test_array.join("")
 end
 
 
