e24a4dd769ee0cfb8ed5f6aca31b9b8ef0f19dac
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-28 04:51:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,5 @@
+reverse_each_word("Hello there, and how are you?")
+
 def reverse_each_word_with_each(string)
 
   original_array = string.split(" ")
@@ -17,6 +19,6 @@ def reverse_each_word(string)
   test_array.join(" ")
 end
 
-reverse_each_word("Hello there, and how are you?")
+
 
   
