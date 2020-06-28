69778977c420bcd27c2b29c50b7db1e4a20d851d
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-06-28 04:53:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 
-
 def reverse_each_word_with_each(string)
 
-  original_array = string(" ")
+  original_array = string.split(" ")
   return_array = []
   original_array.each do|string|
     return_array << string.reverse
@@ -11,7 +10,7 @@ def reverse_each_word_with_each(string)
 end
 
 def reverse_each_word(string)
-  array = string(" ")
+  array = string.split(" ")
   test_array = []
   array.collect do|string|
     test_array << string.reverse
